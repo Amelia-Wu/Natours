94eaee177b9b9a16b2d82f5441a7e9c44881c885
Amelia <xinyi.wu@startupbootcamp.com.au>
2022-12-31 11:59:58 +1100

    connect the MongoDB with Express App

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'})
 
 const app = require('./app');
 
-// console.log(process.env);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('DB connection successful!')
+});
 
 //Start server
 const port = 3000;
