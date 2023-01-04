ec08e18d295f5176c1c0a3cc84963743704227f1
Amelia <xinyi.wu@startupbootcamp.com.au>
2023-01-04 12:12:17 +1100

    improved the API with field limiting and imported all the tours data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'})
+
+const Tour = require('./../../models/tourModel');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('DB connection successful!')
+});
+
+//Read JSON file
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+//Import data into DB
+const importData = async() => {
+    try {
+        await Tour.create(tours);
+        console.log('Data successfully loaded!');
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+//Delete all data from DB
+const deleteData = async() => {
+    try {
+        await Tour.deleteMany();
+        console.log('Data successfully deleted!');
+        process.exit();
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+if (process.argv[2] === '--import') {
+    importData();
+} else if (process.argv[2] === '--delete') {
+    deleteData();
+}
