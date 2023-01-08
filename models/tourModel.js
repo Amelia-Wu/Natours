373f1491ad19f46b14b23fc8d7b9aa1dd1ac665f
Amelia <xinyi.wu@startupbootcamp.com.au>
2023-01-09 10:31:28 +1100

    added validators for the tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,8 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A tour must have a name'],
         unique: true,
-        trim: true
+        trim: true,
+        maxlength: [40, 'A tour name must have less or equal than 40 characters']
     },
     slug: String,
     duration: {
@@ -19,11 +20,16 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour must have a difficulty']
+        required: [true, 'A tour must have a difficulty'],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'Difficulty is either: easy, medium or difficult'
+        }
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Rating must be above 1']
     },
     ratingsQuantity: {
         type: Number,
