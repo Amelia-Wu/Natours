1cc51987e8822ab32f26d3b570e99acc101e30ac
Amelia <xinyi.wu@startupbootcamp.com.au>
2023-01-11 14:30:50 +1100

    signing up users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,24 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 
 exports.signup = catchAsync(async(req, res, next) => {
-    const newUser = await User.create(req.body);
+    //do not simply use req.body because the users can add the role as admin
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+
+    //use the payload and the secret, and the option
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
 
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser
         }
