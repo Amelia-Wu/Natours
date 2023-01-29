604be5ccd7a890cd6ca737f00fa2683913c58a0d
Amelia <xinyi.wu@startupbootcamp.com.au>
2023-01-29 17:00:32 +1100

    Updating the current user: data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {}
+    Object.keys(obj).forEach(el => {
+        if (allowedFields.includes(el)) newObj[el] = obj[el]
+    })
+    return newObj
+}
 
 //Route Handlers
 exports.getAllUsers = catchAsync(async (req, res) => {
@@ -14,6 +23,27 @@ exports.getAllUsers = catchAsync(async (req, res) => {
         }
     })
 });
+
+//The user can update its own data, except for tha password related
+exports.updateMe = catchAsync(async (req, res, next) => {
+    if (req.body.password || req.body.passwordConfirm) {
+       return(next(new AppError('This route is not for password updates. Please use /updateMyPassword.', 400)))
+    }
+
+    //Don't want to update everything in the body, only name and email
+    const filteredBody = filterObj(req.body, 'name', 'email')
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+        new: true,
+        runValidators: true
+    })
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser
+        }
+    })
+})
 
 exports.getUser = (req, res) => {
     res.status(500).json({
