4682b1a513630f4dc1c66e731a00cee016ca1e53
Amelia <xinyi.wu@startupbootcamp.com.au>
2023-02-07 16:02:21 +1100

    Populating reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -24,11 +24,24 @@ const reviewSchema = new mongoose.Schema({
         ref: 'User',
         required: [true, 'Review must belong to a user.']
     }
-},
+    },
     {
         toJSON: {virtuals: true},
         toObject: {virtuals: true}
-    });
+});
+
+//populate the two fields that need reference
+reviewSchema.pre(/^find/, function(next) {
+    this.populate({
+        path: 'tour',
+        select: 'name'
+    }).populate({
+        path: 'user',
+        select: 'name photo'
+    })
+
+    next();
+})
 
 const Review = mongoose.model('Review', reviewSchema);
 
